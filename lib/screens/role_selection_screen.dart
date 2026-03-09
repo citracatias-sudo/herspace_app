b24b76f2411381e8a3ab8237c2b0188519585f9f
citracatias_sudo <citracatias@gmail.com>
2026-03-09 13:47:09 +0700

    register masih error

--- lib/screens/role_selection_screen.dart
+++ lib/screens/role_selection_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:herspace_app/screens/login_screen.dart';
 
-import 'package:herspace_app/screens/register_screen.dart';
 
 class RoleSelectionScreenHp extends StatelessWidget {
   const RoleSelectionScreenHp({super.key});
@@ -55,9 +55,7 @@ class RoleSelectionScreenHp extends StatelessWidget {
                 Navigator.pop(context);
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                    builder: (context) => RegisterScreen(role: 'listener'),
-                  ),
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
                 );
               },
               child: Text("Accept"),
